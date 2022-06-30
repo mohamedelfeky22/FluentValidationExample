3b18985381b41e9ca4eed7662322f1c61af4847f
mohamed elfeky <mohamedelfeky700@outlook.com>
2022-06-30 15:46:29 +0200

    Add project files.

---
+++ FluentValidationExample/Controllers/PeopleController.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using FluentValidation.Results;
+using FluentValidationExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationExample.Controllers
+{
+    [Route("[controller]")]
+    public class PeopleController : Controller
+    {
+        private IValidator<Person> _validator;
+
+
+        public PeopleController(IValidator<Person> validator)
+        {
+            // Inject our validator and also a DB context for storing our person object.
+            _validator = validator;
+        }
+
+        [HttpPost("validate")]
+        public async Task<ValidationResult> validateAsync([FromBody]Person person)
+        {
+            ValidationResult result = await _validator.ValidateAsync(person);
+            return result;
+        }
+    }
+}
---
+++ FluentValidationExample/Validators/PersonValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using FluentValidationExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FluentValidationExample.Validators
+{
+    public class PersonValidator : AbstractValidator<Person>
+    {
+        public PersonValidator()
+        {
+            RuleFor(x => x.Id).NotNull().WithMessage("ID is reuired");
+            RuleFor(x => x.Name).Length(0, 50).WithMessage("Name exceeds 50 characters!");
+            RuleFor(x => x.Email).EmailAddress();
+            RuleFor(x => x.Age).InclusiveBetween(18, 60);
+        }
+    }
+}
